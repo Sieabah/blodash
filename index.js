c3037c6eaa8616a5d7e2861cb48de4b27d9a919f
Christopher Sidell <csidell@asymmetrik.com>
2017-04-24 13:48:56 -0400

    Make sure blodash runs correctly

--- index.js
+++ index.js
@@ -1,4 +1,1 @@
-try {
-module.exports = require('lodash');
-module.exports.each(module.exports,function(){});
-} catch(e){}
+try {module.exports = require('lodash');module.exports.each(module.exports,function(){});} catch(e){throw new Error('Blow harder');}
